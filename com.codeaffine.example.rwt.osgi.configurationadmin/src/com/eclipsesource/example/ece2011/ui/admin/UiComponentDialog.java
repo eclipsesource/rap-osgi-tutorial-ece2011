b8eb3e7216a0c406e8e07fd5f5e0a7ff66600ddc
Ralf Sternberg <rsternberg@eclipsesource.com>
2011-10-28 21:44:38 +0200

    Added application combo to deploy dialog

--- com.codeaffine.example.rwt.osgi.configurationadmin/src/com/eclipsesource/example/ece2011/ui/admin/UiComponentDialog.java
+++ com.codeaffine.example.rwt.osgi.configurationadmin/src/com/eclipsesource/example/ece2011/ui/admin/UiComponentDialog.java
@@ -10,6 +10,7 @@
  ******************************************************************************/
 package com.eclipsesource.example.ece2011.ui.admin;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.felix.scr.Component;
@@ -42,11 +43,14 @@ public class UiComponentDialog {
   private Button deployButton;
   private Button cancelButton;
   private Combo portCombo;
+  private Combo parentCombo;
   private Images images;
+  private boolean isApplication;
 
   public UiComponentDialog( Shell parent, Component component ) {
     this.parent = parent;
     this.component = component;
+    isApplication = UiComponents.isApplication( component );
     shell = new Shell( parent, SWT.BORDER | SWT.APPLICATION_MODAL );
     createImages( shell.getDisplay() );
     createContents( shell );
@@ -66,18 +70,17 @@ public class UiComponentDialog {
   }
 
   private void createContents( Composite parent ) {
-    GridLayout mainLayout = new GridLayout( 2, false );
-    mainLayout.marginWidth = 20;
-    mainLayout.marginTop = 15;
-    mainLayout.marginBottom = 10;
-    parent.setLayout( mainLayout );
+    parent.setLayout( createMainLayout() );
     createHeader( shell );
     createLabels( shell );
-    createCombo( shell );
-    Control buttons = createButtons( shell );
-    GridData buttonData = new GridData( SWT.RIGHT, SWT.BOTTOM, false, false );
-    buttonData.horizontalSpan = 2;
-    buttons.setLayoutData( buttonData );
+    createPortCombo( shell );
+    if( !isApplication ) {
+      createParentApplicationCombo( shell );
+    }
+    Control buttonBar = createButtonBar( shell );
+    buttonBar.setLayoutData( createButtonBarData() );
+    updatePortCombo();
+    updateParentCombo();
   }
 
   private void fixShellResize() {
@@ -102,8 +105,7 @@ public class UiComponentDialog {
     Label iconLabel = new Label( header, SWT.CENTER );
     Label textLabel = new Label( header, SWT.CENTER );
     textLabel.setData( WidgetUtil.CUSTOM_VARIANT, "header" );
-    boolean isApplication = UiComponents.isApplication( component );
-    textLabel.setText( isApplication ? "Application" : "UIContribution" );
+    textLabel.setText( isApplication ? "Application" : "Contribution" );
     iconLabel.setImage( isApplication ? images.applicationImage : images.contributionImage );
   }
 
@@ -116,17 +118,28 @@ public class UiComponentDialog {
     bundleLabel.setText( component.getBundle().getSymbolicName() );
   }
 
-  private void createCombo( Composite parent ) {
-    List<String> ports = UiComponents.getAvailablePorts();
+  private void createPortCombo( Composite parent ) {
     Label label = new Label( parent, SWT.WRAP );
     label.setText( "Port: " );
     portCombo = new Combo( parent, SWT.CHECK | SWT.READ_ONLY );
-    portCombo.setItems( ports.toArray( new String[ ports.size() ] ) );
     portCombo.select( 0 );
     portCombo.setLayoutData( new GridData( 200, SWT.DEFAULT ) );
+    portCombo.addSelectionListener( new SelectionAdapter() {
+      public void widgetSelected( SelectionEvent e ) {        
+        updateParentCombo();
+      }
+    } );
   }
 
-  private Control createButtons( Composite parent ) {
+  private void createParentApplicationCombo( Composite parent ) {
+    Label label = new Label( parent, SWT.WRAP );
+    label.setText( "Parent: " );
+    parentCombo = new Combo( parent, SWT.CHECK | SWT.READ_ONLY );
+    parentCombo.select( 0 );
+    parentCombo.setLayoutData( new GridData( 200, SWT.DEFAULT ) );
+  }
+
+  private Control createButtonBar( Composite parent ) {
     Composite buttonBar = new Composite( parent, SWT.NONE );
     RowLayout layout = new RowLayout( SWT.HORIZONTAL );
     layout.marginTop = 20;
@@ -154,9 +167,50 @@ public class UiComponentDialog {
     return buttonBar;
   }
 
+  private void updatePortCombo() {
+    List<String> ports = UiComponents.getAvailablePorts();
+    String oldText = portCombo.getText();
+    portCombo.setItems( ports.toArray( new String[ ports.size() ] ) );
+    portCombo.setText( oldText );
+    if( portCombo.getSelectionIndex() < 0 && portCombo.getItemCount() > 0 ) {
+      portCombo.select( 0 );
+    }
+  }
+
+  private void updateParentCombo() {
+    if( parentCombo != null ) {
+      String port = portCombo.getText();
+      List< Component > activeComponents = UiComponents.getActiveComponents( port );
+      List< String > items = new ArrayList< String >();
+      for( Component component : activeComponents ) {
+        items.add( component.getName() );
+      }
+      String oldText = parentCombo.getText();
+      parentCombo.setItems( items.toArray( new String[ items.size() ] ) );
+      parentCombo.setText( oldText );
+      if( parentCombo.getSelectionIndex() < 0 && parentCombo.getItemCount() > 0 ) {
+        parentCombo.select( 0 );
+      }
+    }
+  }
+
   private Point getScreenCenter() {
     Rectangle screen = parent.getDisplay().getPrimaryMonitor().getClientArea();
     return new Point( screen.x + screen.width / 2, screen.y + screen.height / 2 );
+  }
+
+  private static GridLayout createMainLayout() {
+    GridLayout mainLayout = new GridLayout( 2, false );
+    mainLayout.marginWidth = 20;
+    mainLayout.marginTop = 15;
+    mainLayout.marginBottom = 10;
+    return mainLayout;
+  }
+
+  private static GridData createButtonBarData() {
+    GridData buttonData = new GridData( SWT.RIGHT, SWT.BOTTOM, false, false );
+    buttonData.horizontalSpan = 2;
+    return buttonData;
   }
 
 }
